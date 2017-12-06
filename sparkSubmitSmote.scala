4f5227810b1715c3a73141e198b700463f957edd
Angkirat Singh Sandhu <angkirat@Angkirats-MBP.Dlink>
2017-12-06 18:30:36 +0530

    Final Smote Class ready

--- sparkSubmitSmote.scala
+++ sparkSubmitSmote.scala
@@ -36,24 +36,61 @@ object smoteClass{
     inputFrame:org.apache.spark.sql.DataFrame,
     feature:String,
     label:String,
-    minorityclass:String,
     percentOver:Int,
     BucketLength:Int,
     NumHashTables:Int):org.apache.spark.sql.DataFrame = {
-    val rowCount = inputFrame.count
+    val groupedData = inputFrame.groupBy(label).count
+    require(groupedData.count == 2, println("Not more or less than 2 labels allowed"))
+    val classAll = groupedData.collect()
+    val minorityclass = if (classAll(0)(1).toString.toInt > classAll(1)(1).toString.toInt) classAll(1)(0).toString else classAll(0)(0).toString
+    val frame = inputFrame.select(feature,label).where(label + " == " + minorityclass)
+    val rowCount = frame.count
     val reqrows = (rowCount * (percentOver/100)).toInt
     val md = udf(smoteCalc _)
-    val b1 = KNNCalculation(inputFrame, feature, reqrows, BucketLength, NumHashTables)
+    val b1 = KNNCalculation(frame, feature, reqrows, BucketLength, NumHashTables)
     val b2 = b1.withColumn("ndtata", md($"k1", $"k2")).select("ndtata")
-    val b3 = b2.withColumn("AllFeatures", explode($"ndtata")).select("AllFeatures").dropDuplicates.withColumn("label", lit(minorityclass))
-    return b3
+    val b3 = b2.withColumn("AllFeatures", explode($"ndtata")).select("AllFeatures").dropDuplicates
+    val b4 = b3.withColumn(label, lit(minorityclass).cast(frame.schema(1).dataType))
+    return inputFrame.union(b4).dropDuplicates
   }
 }
 
-val dataInput = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("/data/sam.csv").na.drop()
-val TFN = "Y"
+def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block    // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0)*1e-9 + "s")
+    result
+}
+
+
+val dataInput = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("/data/train_final.csv").na.drop()
+val TFN = "Target"
 val columnNames = dataInput.columns.filter(_ != TFN)
 val assembler1 = new VectorAssembler().setInputCols(columnNames).setOutputCol("feature")
 val assembled1 = assembler1.transform(dataInput)
 val vectorized = assembled1.select("feature",TFN).withColumn("label",col(TFN)).drop(TFN)
-val newData = Smote(vectorized, "feature", "label", "1", 200, 10, 30)
+time {Smote(vectorized, "feature", "label", 200, 10, 30).count}
+
+
+
+
+val inputFrame = vectorized
+val feature = "feature"
+val label = "label"
+val percentOver = 200
+val BucketLength = 10
+val NumHashTables = 30
+
+val groupedData = inputFrame.groupBy(label).count
+require(groupedData.count == 2, println("Not more or less than 2 labels allowed"))
+val classAll = groupedData.collect()
+val minorityclass = if (classAll(0)(1).toString.toInt > classAll(1)(1).toString.toInt) classAll(1)(0).toString else classAll(0)(0).toString
+val frame = inputFrame.select(feature,label).where(label + " == " + minorityclass)
+val rowCount = frame.count
+val reqrows = (rowCount * (percentOver/100)).toInt
+val md = udf(smoteCalc _)
+val b1 = KNNCalculation(frame, feature, reqrows, BucketLength, NumHashTables)
+val b2 = b1.withColumn("ndtata", md($"k1", $"k2")).select("ndtata")
+val b3 = b2.withColumn("AllFeatures", explode($"ndtata")).select("AllFeatures").dropDuplicates
+val b4 = b3.withColumn(label, lit(minorityclass).cast(frame.schema(1).dataType))
